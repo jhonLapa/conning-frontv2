87b35902cd3190354a5b3b0f778fb6ed9b0b9d98
dev-ronaldino <ronaldinoch30@gmail.com>
2025-10-18 11:58:43 -0500

    mejora categoria

--- src/interfaces/categoria.interface.ts
+++ src/interfaces/categoria.interface.ts
@@ -3,8 +3,30 @@ export interface Categoria {
   nombre: string;
   estado: number;
   fechaCreacion: string;
+
+  conceptosCategoria: ConceptosCategoria[];
+  conceptosCategoriaRequest: ConceptosCategoriaRequest[];
 }
 
 export interface CategoriaRequest {
   nombre: string;
+}
+
+
+export interface ConceptosCategoria {
+  idConcepto: number;
+  idCategoria: number;
+  nombreConcepto: string;
+  valor: number;
+  estado: number;
+  fechaCreacion: string;          // o Date si luego la manejas como objeto de fecha
+  usuarioCreacion: string;
+  fechaCambioEstado: string | null;  // null si aún no hay fecha
+  usuarioCambioEstado: string | null;
+}
+
+export interface ConceptosCategoriaRequest {
+  idCategoria: number;
+  nombreConcepto: string;
+  estado: number;
 }
